42fd54df417c0cf1e8fe1d5dfba59e8c5f287915
TamySihotang <sihotangtamy@gmail.com>
2016-06-23 19:35:10 +0700

    second commit

--- protected/controllers/SparepartController.php
+++ protected/controllers/SparepartController.php
@@ -32,7 +32,7 @@ class SparepartController extends Controller
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array('create','update','addSparepart'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
@@ -78,30 +78,7 @@ class SparepartController extends Controller
 			'model'=>$model,
 		));
 	}
-
-	/**
-	 * Updates a particular model.
-	 * If update is successful, the browser will be redirected to the 'view' page.
-	 * @param integer $id the ID of the model to be updated
-	 */
-	public function actionUpdate($id)
-	{
-		$model=$this->loadModel($id);
-
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-
-		if(isset($_POST['sparepart']))
-		{
-			$model->attributes=$_POST['sparepart'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
-		}
-
-		$this->render('update',array(
-			'model'=>$model,
-		));
-	}
+        
 
 	/**
 	 * Deletes a particular model.
